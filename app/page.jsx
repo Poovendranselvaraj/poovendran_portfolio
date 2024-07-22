9af011dabca5c4a67552dc2a9d410b552e08acd3
poovendran <poovendranselvaraj333@gmail.com>
2024-07-22 17:36:44 +0530

    3.26.26

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,11 @@
 //components
 import About from "@/components/About";
+import Cta from "@/components/Cta";
 import Hero from "@/components/Hero";
+import Reviews from "@/components/Reviews";
 import Services from "@/components/Services";
 import Work from "@/components/Work";
+
 
 export default function Home() {
   return (
@@ -11,6 +14,8 @@ export default function Home() {
       <About />
       <Services />
       <Work />
+      <Reviews />
+      <Cta />
     </main>
   );
 }
--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,9 +1,115 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
+import {Tabs, TabsList,TabsContent,} from '@/components/ui/tabs'
+import ProjectCard from '@/components/ProjectCard'
+
+const ProjectData = [
+  {
+      image: '/work/3.png',
+      category:'react js',
+      name:'Nexa Website',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, quis.',
+      link: '/',
+      github:'/',
+  },
+  {
+      image: '/work/4.png',
+      category:'react js',
+      name:'Solstice Website',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, quis.',
+      link: '/',
+      github:'/',
+  },
+  {
+      image: '/work/2.png',
+      category:'next js',
+      name:'Lumina Website',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, quis.',
+      link: '/',
+      github:'/',
+  },
+  {
+      image: '/work/1.png',
+      category:'next js',
+      name:'Evolve Website',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, quis.',
+      link: '/',
+      github:'/',
+  },
+  {
+      image: '/work/3.png',
+      category:'next js',
+      name:'Ignite Website',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, quis.',
+      link: '/',
+      github:'/',
+  },
+  {
+      image: '/work/4.png',
+      category:'next js',
+      name:'Envision Website',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, quis.',
+      link: '/',
+      github:'/',
+  },
+  {
+      image: '/work/1.png',
+      category:'fullstack',
+      name:'Serenity Website',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, quis.',
+      link: '/',
+      github:'/',
+  },
+  {
+      image: '/work/3.png',
+      category:'fullstack',
+      name:'Nova Website',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, quis.',
+      link: '/',
+      github:'/',
+  },
+  {
+      image: '/work/2.png',
+      category:'fullstack',
+      name:'Zenith Website',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, quis.',
+      link: '/',
+      github:'/',
+  },
+]
+
+//remove category duplicates
+const uniqueCategories = [
+  'all projects',
+  ...new Set(ProjectData.map((item) => item.category)),
+];
 
 const Projects = () => {
-  return (
-    <div>Projects Page</div>
-  )
+  const [categories, setCategories] = useState(uniqueCategories);
+  console.log(categories);
+  return <section className="min-h-screen pt-12">
+    <div className='container mx-auto'>
+      <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">
+        My Projects
+      </h2>
+      {/*tabs*/}
+      <Tabs>
+        <TabsList>
+      </Tabs>
+    </div>
+      
+    
+    </section>
+  
 }
 
 export default Projects;
