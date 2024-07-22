5c9bf17befe6430d8ca883b58d6f8d9331f10b7e
poovendran <poovendranselvaraj333@gmail.com>
2024-07-22 19:05:16 +0530

    error

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,9 +1,9 @@
 'use client'
 import React, { useState } from 'react'
-import {Tabs, TabsList,TabsContent,} from '@/components/ui/tabs'
+import {Tabs, TabsList,TabsContent, TabsTrigger} from '@/components/ui/tabs'
 import ProjectCard from '@/components/ProjectCard'
 
-const ProjectData = [
+const projectData = [
   {
       image: '/work/3.png',
       category:'react js',
@@ -90,26 +90,55 @@ const ProjectData = [
 //remove category duplicates
 const uniqueCategories = [
   'all projects',
-  ...new Set(ProjectData.map((item) => item.category)),
+  ...new Set(projectData.map((item) => item.category)),
 ];
 
 const Projects = () => {
   const [categories, setCategories] = useState(uniqueCategories);
-  console.log(categories);
-  return <section className="min-h-screen pt-12">
-    <div className='container mx-auto'>
-      <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">
-        My Projects
-      </h2>
-      {/*tabs*/}
-      <Tabs>
-        <TabsList>
-      </Tabs>
-    </div>
-      
-    
+  const [category, setCategory] = useState('all projects');
+
+  const filteredProjects=projectData.filter(project=>{
+    //if category is 'all projects', return all projects, else filter by category
+    return category === 'all projects' 
+      ? project 
+      : project.category === category;
+  })
+  return (
+    <section className="min-h-screen pt-12">
+      <div className='container mx-auto'>
+        <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">
+          My Projects
+        </h2>
+        {/*tabs*/}
+        <Tabs defaultValue={category} className='mb-24 xl:mb-48'>
+          <TabsList className='w-full grid h-full md:grid-cols-4 lg:max-w-[640px] mb-12 mx-auto md:border dark:border-none'>
+           {categories.map((category, index) => {
+              return (
+                <TabsTrigger 
+                  onClick={() => setCategory(category)}
+                  value={category} 
+                  key={index} 
+                  className='capitalize w-[162px] md:w-auto'
+                >
+                  {category}
+                </TabsTrigger>
+              )
+            })} 
+          </TabsList>
+          {/*tabs content*/}
+          <div className='text-lg xl:mt-8 grid grid-cols-1 lg:grid-cols-3 gap-4'>
+            {filteredProjects.map((project,index)=> {
+              return (
+                <TabsContent value={category} key={index}>
+                  <ProjectCard project={project}/>
+                </TabsContent>
+              )
+            })}
+          </div>
+        </Tabs>
+      </div>
     </section>
-  
+  )
 }
 
 export default Projects;
