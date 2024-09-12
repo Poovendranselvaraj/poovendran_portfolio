7cc7366d65267c9b4bacfb1a60f1f62e546727a7
Poovendranselvaraj <poovendranselvaraj333@gmail.com>
2024-09-12 22:33:14 +0530

    added taget blank

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -11,23 +11,24 @@ const ProjectCard = ({project}) => {
             {/*image*/}
             <div className="relative w-full h-[300px] flex items-center justify-center
             bg-tertiary dark:bg-secondary/40 xl:bg-work_project_bg_light xl:dark:bg-work_project_bg_dark xl:bg-[110%] xl:bg-no-repeat overflow-hidden">
-                <Image 
-                  className="absolute bottom-0 shadow-2xl" 
-                  src={project.image}
-                  width={247}
-                  height={250}
-                  alt=''
-                  priority
-                />
+  <Image 
+    className="absolute bottom-0 shadow-2xl object-cover" 
+    src={project.image}
+    alt=''
+    fill
+    priority
+  />
+
                 {/*btn links*/}
                 <div className="flex gap-x-4">
                 <Link 
+                target="_blank"
                       href={project.link} 
                       className="bg-secondary w-[54px] h-[54px] rounded-full flex justify-center items-center scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100 transition-all duration-200"
                     >
                       <Link2Icon className="text-white" />
                     </Link>
-                    <Link 
+                    <Link  target="_blank"
                       href={project.github} 
                       className="bg-secondary w-[54px] h-[54px] rounded-full flex justify-center items-center scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100 transition-all duration-300"
                     >
