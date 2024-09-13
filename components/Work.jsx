4c28373483aeabcd5e696fd4d5acf152f6bc0b66
Poovendranselvaraj <poovendranselvaraj333@gmail.com>
2024-09-13 12:58:37 +0530

    add swip

--- components/Work.jsx
+++ components/Work.jsx
@@ -20,19 +20,19 @@ import ProjectCard from '@/components/ProjectCard'
 const projectData = [
   {
     image: '/work/React.png',
-    category: 'react js',
+    category: 'fullstack',
     name: 'React Token Based Authentication app',
     description:
-      'Jwt token based authentication, with refresh token based authentication, and redux for state management Used custom hooks for session and local storage Developed using React for a dynamic UI, React Hook Form for efficient form handling, Yup for validation, and  Styled using CSS.npm: Managed project dependencies and scripts using npm, streamlining the development process.Built with Node.js and Express.js for server - side logic, integrated with a database for data storage.',
+      'This project is a token-based authentication app built with React, using JWT for authentication and refresh tokens for enhanced security. It incorporates Redux for state management, React Hook Form for form handling, and Yup for validation. The UI is styled with CSS. The server side is powered by Node.js and Express.js, connected to a database for data storage. The project was managed with npm, streamlining the development process. Languages/technologies used: JavaScript, React, Node.js, Express.js, CSS, and npm.',
     link: '',
     github: 'https://github.com/Poovendranselvaraj/react-js-validation',
   },
   {
     image: '/work/Hospital.png',
     category: 'react js',
-    name: 'Hospital Management',
+    name: 'Hospital Management System',
     description:
-      'Build a healthcare platform that streamlines patient registration.Appointment scheduling, and medical records.Learn to implement complex forms and SMS notifications.User Profiles: Patients can create accounts with personal details, health history, and contact information.Appointment Confirmation: Send automatic SMS and email notifications to confirm appointments.Test Results Notifications: Notify patients when test results are available for viewing.Prescription Reminders: Send medication reminders via SMS for prescription adherence.',
+      'This Hospital Management project is a healthcare platform that simplifies patient registration, appointment scheduling, and medical records. It features user profiles, automatic SMS/email notifications for appointments, test results, and prescription reminders to ensure patient adherence.Languages/technologies used: JavaScript, Node.js, React, and SMS/email integration services.',
     link: 'https://hospital-jade.vercel.app/',
     github: 'https://github.com/Poovendranselvaraj/Hospital',
   },
@@ -41,7 +41,7 @@ const projectData = [
     category: 'react js',
     name: 'React Fitness app',
     description:
-      'Designed and developed a gym application using React and javaScript.This app consists of two pages Home and Exercises.Built and deployed in netlify and also dockerized.This React Fitness App is a streamlined and user-friendly platform designed to help gym users manage and track their workout routines. The app focuses on two primary sections: Home and Exercises, offering users an intuitive interface to browse exercise guides, track their fitness progress, and receive workout recommendations. The app is fully responsive and works seamlessly across devices.',
+      'This React Fitness App is designed to help users manage and track their workouts. It features a Home page and an Exercises section, providing exercise guides, progress tracking, and workout recommendations. The app is fully responsive, deployed on Netlify, and Dockerized for easy deployment.Languages/technologies used: JavaScript, React, Netlify.',
     link: 'https://poovis-gym.netlify.app',
     github: 'https://github.com/Poovendranselvaraj/gym-app-react',
   },
@@ -51,7 +51,7 @@ const projectData = [
     category: 'fullstack',
     name: 'Mern Todo-list',
     description:
-      'A Backend server made using HTML,CSS and javaScript.You can add works to be done and can be deleted in this and can be removed.The MERN Todo List is a full-stack application where users can:Add new tasks to be done,Delete tasks,Mark tasks as completed,View all tasks.This project uses the following technologies:MongoDB: For storing todo tasks,Express.js: For handling the backend API routes,React.js: For building the user interface To run the server and handle API requests.User Authentication: Integrate user login and registration (using JWT).',
+      'The MERN Todo List is a full-stack app where users can add, delete, and track tasks. It supports user authentication with JWT and allows task management via a simple UI.Languages/technologies used: MongoDB, Express.js, React.js, Node.js, HTML, CSS, and JavaScript.',
     link: 'https://poovetodo.netlify.app/',
     github: 'https://github.com/Poovendranselvaraj/todo_list',
   },
@@ -60,16 +60,16 @@ const projectData = [
     category: 'Backend',
     name: 'FastApi Python',
     description:
-      'The Proficient in designing robust backend services,integrating relational databases,and automating testing using Postman. Strong skills in containerizing applications with Docker and managing databases with PgAdmin.Developed APIs using FastAPI, ensuring performance and security.Implemented CRUD operations with PostgreSQL,managed via PgAdmin.Utilized Postman for API testing and validation.Containerized applications with Docker,Integrated data validation and serialization with Pydantic models.',
+      'This project involves building robust backend services using FastAPI, with CRUD operations handled by PostgreSQL and managed via PgAdmin. APIs are tested and validated using Postman, and applications are containerized with Docker. Pydantic models are used for data validation and serialization.Languages/technologies used: Python, FastAPI, PostgreSQL, Docker, PgAdmin, and Postman.',
     link: '',
     github: 'https://github.com/Poovendranselvaraj/fastapi',
   },
   // {
-  //   image: '/work/Flask-todo.png',
+  //   image: '/work/Flask-Todo.png',
   //   category: 'Mini project',
   //   name: 'Flask Todo-list',
   //   description:
-  //     'Built a full-featured To-Do List application using Flask, focusing on clean architecture, database management, and frontend integration. creating RESTful APIs, handling user authentication, and developing CRUD functionality.Developed a Flask-based To-Do List web application with user authentication Implemented CRUD operations for tasks, Integrated HTML, CSS, and JavaScript for an intuitive user interface.Ensured secure data handling with Flask-WTF and Flask-Login for form validation and user sessionsTested and debugged using Postman for API endpoints..',
+  //     'The Flask Todo List is a web application with user authentication and task management. It features CRUD operations, a user-friendly UI, and secure data handling with Flask-WTF and Flask-Login. APIs were tested using Postman.Languages/technologies used: Python, Flask, HTML, CSS, JavaScriptand Postman.',
   //   link: 'https://flask-todo-8uvr.onrender.com/',
   //   github: 'https://github.com/Poovendranselvaraj/Flask-todo',
   // },
