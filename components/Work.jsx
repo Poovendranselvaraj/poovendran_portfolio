bcccd7174496f1b8b934607e370e3665a6fc194c
Poovendranselvaraj <poovendranselvaraj333@gmail.com>
2024-09-13 13:41:13 +0530

    add swip

--- components/Work.jsx
+++ components/Work.jsx
@@ -101,7 +101,7 @@ const Work = () => {
                  pagination={{clickable:true}}
                 >
                     {/*show only the first 4 projects for the slides*/}
-                    {projectData.slice(0, 4).map((project, index) => {
+                    {projectData.slice(0, 5).map((project, index) => {
                         return (
                             <SwiperSlide key={index}>
                                 <ProjectCard project={project}/>
