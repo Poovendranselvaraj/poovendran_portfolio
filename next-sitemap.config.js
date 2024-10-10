ad7b62d29e4a4cfa0196fd0f265db53ceb19d0c4
Poovendranselvaraj <poovendranselvaraj333@gmail.com>
2024-10-10 20:45:37 +0530

    add site verify

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,16 @@
+const nextConfig = {
+  // Other configurations...
+  async head() {
+    return [
+      {
+        key: 'google-site-verification',
+        value: 'googlef175438ea0990b88.html',
+      },
+    ];
+  },
+};
+
 module.exports = {
-    siteUrl: 'https://poovendran.netlify.app',
-    generateRobotsTxt: true,
-  };
-  
+  siteUrl: 'https://poovendran.netlify.app',
+  generateRobotsTxt: true,
+};
