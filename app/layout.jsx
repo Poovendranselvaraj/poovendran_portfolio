c1b679e9da17b438d49f9d0748f4603ec31a306c
Poovendranselvaraj <poovendranselvaraj333@gmail.com>
2024-10-18 09:47:09 +0530

    updated navbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,7 +14,7 @@ const outfit = Outfit({ subsets: ["latin"] });
 export const metadata = {
   title: "Poovendran S | Full Stack Developer Portfolio",
   description: "Poovendran S, Full Stack Developer specializing in React, Node.js, and MongoDB. Explore my projects, skills, and achievements.",
-  keywords: "Poovendran S, Full Stack Developer, React, Node.js, MongoDB, Portfolio",
+  keywords: "Poovendran S, Full Stack Developer, React, Node.js, MongoDB, Portfolio,Poovendran Selvaraj",
 };
 
 // Root Layout Component
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="Poovendran S, Full Stack Developer specializing in React, Node.js, and MongoDB. Explore my projects, skills, and achievements." />
-        <meta name="keywords" content="Poovendran S, Full Stack Developer, React, Node.js, MongoDB, Portfolio" />
+        <meta name="keywords" content="Poovendran S, Full Stack Developer, React, Node.js, MongoDB, Portfolio,Poovendran Selvaraj" />
       </head>
       <body className={outfit.className}>
         <ThemeProvider attribute="class" defaultTheme="light">
